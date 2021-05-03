9d103530c2b39ea35cd674a5a364b3bff60d4d93
nuke-droid <33710624+nuke-droid@users.noreply.github.com>
2021-05-03 08:41:47 -0700

    Update MassInvoke.ps1

--- MassInvoke.ps1
+++ MassInvoke.ps1
@@ -1,9 +1,24 @@
 
-
+$Form1 = New-Object -TypeName System.Windows.Forms.Form
+function InitializeComponent
+{
+$Form1.SuspendLayout()
+#
+#Form1
+#
+$Form1.Text = [System.String]'Mass Invoke'
+$Form1.ResumeLayout($false)
+Add-Member -InputObject $Form1 -Name base -Value $base -MemberType NoteProperty
+}
+. InitializeComponent
 Add-Type -AssemblyName System.Windows.Forms
+
+
+
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
 $initialDirectory = "C:\"
+$global:computerslist 
 Function Get-FileName($initialDirectory)
 {  
     
@@ -16,26 +31,44 @@ Function Get-FileName($initialDirectory)
  $OpenFileDialog.ShowDialog() | Out-Null
  $OpenFileDialog.filename
  
-} #end function Get-FileName
+} #end function Get-FileName 
 
 function Establish-Session (){
 
-    foreach ($computer in $TextBox1.Text){
-        $credential = Get-Credential
-        
-        if($session.State -eq "Opened"){
-            $session = New-PSSession -ComputerName $computer -Credential $credential 
-            Invoke-Command -Session $session -ScriptBlock {$TextBox2.Text}
-        }
-        else{
-            Write-Host "Connection Failed"
-        }
-
-        
-
-    }
+    $computers = Get-Content -Path $global:computerslist   
     
+    foreach ($computer in $computers)
+{
+$session = New-PSSession -Computer $computer
+
+	if($session.State -eq "Opened")
+	{
+        Write-Host ""
+
+		#Initiate remote session, pass in credentials and commands, find all bilockered volumes (not just C:\)
+		Invoke-Command -Session $session -ScriptBlock {ping AI911420}
+
+		
+
+        Get-PSSession -Computer $computer | Remove-PSSession
+
+        if($session.State -eq "Opened")
+        {
+        Write-Host "$computer session not closed" -ForegroundColor DarkRed
+        }
+        else
+        {
+        Write-Host "$computer session closed successfully" -ForegroundColor DarkGreen
+        }
+    }
+	else
+	{
+	Write-Host ""
+        Write-Host "$computer is inaccessible" -ForegroundColor Red
+    }
 }
+}
+
 
 $MassInvoke                      = New-Object system.Windows.Forms.Form
 $MassInvoke.ClientSize           = New-Object System.Drawing.Point(858,662)
@@ -54,7 +87,7 @@ $Button1.BackColor               = [System.Drawing.ColorTranslator]::FromHtml("#
 $Button1.Add_click({
     $selFile = Get-FileName
     $Import = Get-Content -Path $selFile -Delimiter " "
-    
+    $global:computerslist = $selFile
     $TextBox1.Text = $Import
 }
 )
@@ -66,14 +99,18 @@ $TextBox1.multiline              = $true
 $TextBox1.width                  = 221
 $TextBox1.height                 = 518
 $TextBox1.location               = New-Object System.Drawing.Point(53,45)
-$TextBox1.Font                   = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$TextBox1.Font                   = New-Object System.Drawing.Font('Courier New',10)
+$TextBox1.BackColor              =[System.Drawing.ColorTranslator]::FromHtml("Black")
+$TextBox1.ForeColor              =[System.Drawing.ColorTranslator]::FromHtml("Green")
 
 $TextBox2                        = New-Object system.Windows.Forms.TextBox
 $TextBox2.multiline              = $true
 $TextBox2.width                  = 482
 $TextBox2.height                 = 140
 $TextBox2.location               = New-Object System.Drawing.Point(326,423)
-$TextBox2.Font                   = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$TextBox2.Font                   = New-Object System.Drawing.Font('Courier New',10)
+$TextBox2.BackColor              =[System.Drawing.ColorTranslator]::FromHtml("Black")
+$TextBox2.ForeColor              =[System.Drawing.ColorTranslator]::FromHtml("Green")
 
 $Button2                         = New-Object system.Windows.Forms.Button
 $Button2.text                    = "Import Script"
@@ -90,12 +127,16 @@ $Button2.Add_click({
 }
 )
 
-$Groupbox1                       = New-Object system.Windows.Forms.Groupbox
-$Groupbox1.height                = 351
-$Groupbox1.width                 = 481
-$Groupbox1.text                  = "Group Box"
-$Groupbox1.location              = New-Object System.Drawing.Point(326,45)
-$Groupbox1.BackColor             = [System.Drawing.ColorTranslator]::FromHtml("#147fe6")
+$TextBox3                       = New-Object System.Windows.Forms.TextBox
+$TextBox3.multiline             = $true
+$TextBox3.height                = 351
+$TextBox3.width                 = 481
+$TextBox3.text                  = Out-Host
+$TextBox3.location              = New-Object System.Drawing.Point(326,45)
+$TextBox3.BackColor             = [System.Drawing.ColorTranslator]::FromHtml("Black")
+$TextBox3.ForeColor             = [System.Drawing.ColorTranslator]::FromHtml("Green")
+$TextBox3.ScrollBars            = "Vertical"
+
 
 $Button3                         = New-Object system.Windows.Forms.Button
 $Button3.text                    = "Execute"
@@ -106,9 +147,9 @@ $Button3.Font                    = New-Object System.Drawing.Font('Consolas',12,
 $Button3.BackColor               = [System.Drawing.ColorTranslator]::FromHtml("#03d718")
 $Button3.Add_Click({Establish-Session})
 
-$MassInvoke.controls.AddRange(@($Button1,$TextBox1,$TextBox2,$Button2,$Groupbox1,$Button3))
+$MassInvoke.controls.AddRange(@($Button1,$TextBox1,$TextBox2,$Button2,$TextBox3,$Button3))
 
-$Groupbox1.Text = 
+
 
 
 $MassInvoke.ShowDialog()
@@ -123,6 +164,11 @@ $MassInvoke.Controls.Add($Button1)
         
         
         
+
+
+
+
+
 
 
 
